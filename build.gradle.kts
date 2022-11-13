261607e5b50a953c580603c507147ed4f221afca
Régis Dika <regis-971@hotmail.com>
2022-11-13 16:28:49 +0100

    add dependency

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("7.2.2").apply(false)
+    id("com.android.library").version("7.2.2").apply(false)
+    id("com.squareup.sqldelight").version("1.5.3").apply(false)
+    id("com.google.dagger.hilt.android").version("2.42").apply(false)
+    kotlin("android").version("1.7.10").apply(false)
+    kotlin("multiplatform").version("1.7.10").apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
